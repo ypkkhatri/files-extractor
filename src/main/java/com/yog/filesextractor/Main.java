182860994fc0a85d499b109c1867b61fd2974f79
Yougeshwar Khatri <ypkkhatri@gmail.com>
2023-07-08 10:28:40 +0200

    Initial commit

---
+++ src/main/java/com/yog/filesextractor/Main.java
@@ -0,0 +1,12 @@
+package com.yog.filesextractor;
+
+/**
+ *
+ * @author Yougeshwar
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
